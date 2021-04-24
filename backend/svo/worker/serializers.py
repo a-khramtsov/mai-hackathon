b658497e78c9b2951b3923578d94cb9867e064e3
Anton Shcherbak <sherback.anton@gmail.com>
2021-04-25 02:44:42 +0300

    new serializers

--- backend/svo/worker/serializers.py
+++ backend/svo/worker/serializers.py
@@ -1,3 +1,5 @@
+from django.utils import timezone
+
 from rest_framework import serializers
 
 from core.models import Application, Resource
@@ -15,3 +17,16 @@ class ResourcesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Resource
         fields = "__all__"
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        application = instance.resource_applications.\
+            filter(end_time__gte=timezone.now()).order_by('-end_time').first()
+        if application:
+            data['blocked'] = True
+            data['blocked_until'] = str(application.end_time)
+        else:
+            data['blocked'] = False
+            data['blocked_until'] = None
+
+        return data
