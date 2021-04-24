eba84803132e1de381728d656856a3f3757a2631
Pavel Lapotnikov <38629071+lpvmak@users.noreply.github.com>
2021-04-25 02:28:58 +0300

    Added parking

--- backend/svo/svo/settings.py
+++ backend/svo/svo/settings.py
@@ -146,6 +146,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = Path(BASE_DIR, 'static')
-
+MEDIA_URL = '/static/'
+MEDIA_ROOT = Path(BASE_DIR, 'static')
 CORS_ORIGIN_ALLOW_ALL = True # If this is used then `CORS_ORIGIN_WHITELIST` will not have any effect
 CORS_ALLOW_CREDENTIALS = True
