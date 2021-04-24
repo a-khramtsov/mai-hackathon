1471d2e0618a603e0cc10a517aca4ec4a3ebf1e5
Pavel Lapotnikov <38629071+lpvmak@users.noreply.github.com>
2021-04-24 21:05:00 +0300

    Added initial base models

--- backend/svo/svo/urls.py
+++ backend/svo/svo/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/worker/', include('worker.urls')),
+    path('api/worker/', include('dispatcher.urls')),
     path('api/airline/', include('airline.urls')),
     path('api/dispatcher/', include('worker.urls')),
     path('api/core/', include('core.urls')),
