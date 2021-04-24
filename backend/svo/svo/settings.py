f6582a2f13f08e81d02243d2180df2785f32b15d
Pavel Lapotnikov <38629071+lpvmak@users.noreply.github.com>
2021-04-24 23:35:41 +0300

    Added filters and schema

--- backend/svo/svo/settings.py
+++ backend/svo/svo/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '@bdm3e_9u)im83u3ee&198nk)^qapv_z&t7&x6va6r5-b7xbd!'
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'http://api.svo.lapotnikov.ru'
+    'api.svo.lapotnikov.ru'
 ]
 
 # Application definition
