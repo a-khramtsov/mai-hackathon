6a440418bba4a387abb6e65960e546b7ee877b9f
Anton Shcherbak <sherback.anton@gmail.com>
2021-04-24 18:41:33 +0300

    worker endpoints update

--- backend/svo/worker/urls.py
+++ backend/svo/worker/urls.py
@@ -1,3 +1,12 @@
+from rest_framework import routers
+
+from . import views
+
+router = routers.SimpleRouter()
+router.register(r"applications", views.ApplicationViewSet)
+
+
 urlpatterns = [
 
 ]
+urlpatterns += router.urls
--- backend/svo/worker/views.py
+++ backend/svo/worker/views.py
@@ -1,5 +1,15 @@
-from rest_framework import generics, viewsets, status
+from rest_framework import generics, viewsets, status, mixins, permissions
 from rest_framework.response import Response
 
 from . import models, serializers
+from core.models import Application
 
+
+class ApplicationViewSet(mixins.ListModelMixin,
+                         mixins.CreateModelMixin,
+                         mixins.RetrieveModelMixin,
+                         viewsets.GenericViewSet):
+
+    queryset = Application.objects.all()
+    serializer_class = serializers.ApplicationSerializer
+    permission_classes = [permissions.IsAuthenticated]
