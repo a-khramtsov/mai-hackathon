306b13a696dcecdcf306a965511682aeeeb9ee68
Pavel Lapotnikov <38629071+lpvmak@users.noreply.github.com>
2021-04-25 13:35:19 +0300

    Added estimation

--- backend/svo/svo/asgi.py
+++ backend/svo/svo/asgi.py
@@ -5,11 +5,12 @@ from channels.http import AsgiHandler
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.urls import re_path
 
-from airline.consumers import AirlineConsumer
-from dispatcher.consumers import DispatcherConsumer
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'svo.settings')
 django.setup()
+
+from airline.consumers import AirlineConsumer
+from dispatcher.consumers import DispatcherConsumer
 
 application = ProtocolTypeRouter(dict(http=URLRouter([
     re_path(r"", AsgiHandler()),
